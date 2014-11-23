24b1741b80bd082bfba9ebd1a5576701857e30ef
ReyNicolas <rey.nicolas.92@gmail.com>
2014-11-23 19:06:31 -0300

    ledercastyForm

--- FrbaHotel/FrbaHotel/Listado Estadistico/Form1.Designer.cs
+++ FrbaHotel/FrbaHotel/Listado Estadistico/Form1.Designer.cs
@@ -28,11 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(350, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Mostar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Año";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numero de Trimestre";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(127, 130);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(86, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Listado TOP 5 ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(384, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(515, 188);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 262);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
     }
 }
--- FrbaHotel/FrbaHotel/Listado Estadistico/Form1.cs
+++ FrbaHotel/FrbaHotel/Listado Estadistico/Form1.cs
@@ -14,6 +14,112 @@ namespace FrbaHotel.Listado_Estadistico
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Items.Add("1");
+            comboBox1.Items.Add("2");
+            comboBox1.Items.Add("3");
+            comboBox1.Items.Add("4");
+            comboBox2.Items.Add("Hoteles con mayor cantidad de reservas canceladas");
+            comboBox2.Items.Add("Hoteles con mayor cantidad de consumibles facturados.");
+            comboBox2.Items.Add("Hoteles con mayor cantidad de días fuera de servicio");
+            comboBox2.Items.Add("Habitaciones con mayor cantidad de días y veces que fueron ocupadas");
+            comboBox2.Items.Add("Cliente con mayor cantidad de puntos");
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fechaInicial;
+            string fechaFinal;
+            string busqueda;
+
+            if (comboBox1.SelectedItem =="1")
+            {
+                    fechaInicial = "'"+ textBox1.Text + "-01-01";          
+                    fechaFinal = "'" + textBox1.Text + "-03-31"; 
+            }
+            if (comboBox1.SelectedItem == "2")
+            {
+                 fechaInicial = "'" + textBox1.Text + "-04-01";          
+                 fechaFinal = "'" + textBox1.Text + "-06-30";
+            }
+            if (comboBox1.SelectedItem == "3")
+            {
+                 fechaInicial = "'" + textBox1.Text + "-07-01";         
+                 fechaFinal = "'" + textBox1.Text + "-09-30";
+            }
+            if (comboBox1.SelectedItem == "4")
+            {
+                 fechaInicial = "'" + textBox1.Text + "-10-01";          
+                 fechaFinal = "'" + textBox1.Text + "-12-31";
+            }
+
+
+            if (comboBox2.SelectedItem == "Hoteles con mayor cantidad de reservas canceladas")
+            {
+                 busqueda =    "SELECT * FROM   vistaTOP5ReservasCanceladas('"+ fechaInicial + "','"+ fechaFinal +"')"
+            }
+
+
+
+            if (comboBox2.SelectedItem == "Hoteles con mayor cantidad de consumibles facturados")
+            {
+                 busqueda =    "SELECT * FROM   vistaTOP5ConsumiblesFacturados('"+ fechaInicial + "','"+ fechaFinal +"')"
+            }
+
+            if (comboBox2.SelectedItem == "Hoteles con mayor cantidad de días fuera de servicio")
+                            {
+                 busqueda =    "SELECT * FROM   vistaTOP5CantidadDeDiasFueraDeServicio('"+ fechaInicial + "','"+ fechaFinal +"')"
+            }
+
+
+            if (comboBox2.SelectedItem == "Habitaciones con mayor cantidad de días y veces que fueron ocupadas")
+                            {
+                 busqueda =    "SELECT * FROM   vistaTOP5HabitacionesHabitadas('"+ fechaInicial + "','"+ fechaFinal +"')"
+            }
+
+
+            if (comboBox2.SelectedItem == "Cliente con mayor cantidad de puntos")
+                         {
+                 busqueda =    "SELECT * FROM   vistaTOP5ClienteConPuntos('"+ fechaInicial + "','"+ fechaFinal +"')"
+            }   
+          
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
